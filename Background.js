30f6fe32ffc8db6aca762fc34d0bbddac7b060ec
Ahmed Gamal <ahmed.gamalhamed5@gmail.com>
2024-01-23 11:27:10 +0200

    added chatgpt answer

--- Background.js
+++ Background.js
@@ -1,4 +1,10 @@
 let Binds = {};
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const file = Object.values(Binds)[0];
+  console.log(file, tab);
+  if (file && tab.id) runfileHere(tab.id, file);
+});
+
 chrome.storage.local.get("Binds", (res) => (Binds = res.Binds));
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -6,14 +12,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   const order = request.order;
   if (order == "Macro") {
     const key = request.key;
-    const shift = request.shift;
-    const ctrl = request.ctrl;
-    if (key && ctrl && shift) {
-      let tabId = sender.tab.id;
-      if (Binds[key]) {
-        runfileHere(tabId, Binds[key]);
-      }
+    // if (key && ctrl && shift) {
+    let tabId = sender.tab.id;
+    if (Binds[key]) {
+      runfileHere(tabId, Binds[key]);
     }
+    // }
   } else if (order == "update") {
     Binds = request.Binds;
     chrome.storage.local.set({ Binds });
--- listeners.js
+++ listeners.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", initListeners());
 
-async function initListeners() {
-  document.addEventListener("keypress", (e) => {
-    let key = e.code[3];
-    let ctrl = e.ctrlKey;
-    let shift = e.shiftKey;
-    if (key && ctrl && shift) {
-      chrome.runtime.sendMessage({ order: "Macro", key: key, shift: shift, ctrl: ctrl });
-      console.log(key, ctrl, shift);
+function listen(e) {
+  let key = e.code[3];
+  let ctrl = e.ctrlKey;
+  let shift = e.shiftKey;
+  let alt = e.altKey;
+  if ((key && ctrl && shift) || (key && alt && ctrl)) {
+    if (key == "E") {
+      console.log("refreshing listenr");
+      document.removeEventListener("keydown", listen);
+      document.addEventListener("keydown", listen);
     }
-  });
+    chrome.runtime.sendMessage({ order: "Macro", key: key, shift: shift, ctrl: ctrl });
+    console.log(key, ctrl, shift, alt);
+  }
+}
+
+async function initListeners() {
+  document.addEventListener("keydown", listen);
 }
